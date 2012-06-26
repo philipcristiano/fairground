7831b1149d5833d34959743fde536773f3449f45
Philip Cristiano <philipcristiano@gmail.com>
2012-06-26 18:10:21 -0400

    prototype: Run command in an arbiter

--- fairground/proto_watcher.py
+++ fairground/proto_watcher.py
@@ -1,17 +1,50 @@
 from kazoo.client import KazooClient
 import zookeeper
 import time
+from circus import get_arbiter
+from circus.client import CircusClient
+from multiprocessing import Process
 
+def start_arbiter():
+    arbiter = get_arbiter([])
+    print 'starting arbiter'
+    try:
+        arbiter.start()
+    finally:
+        arbiter.stop()
 
-client = KazooClient('33.33.33.10:2181')
+p = Process(target=start_arbiter)
+p.start()
 
-client
-client.connect()
+zookeeper_client = KazooClient('33.33.33.10:2181')
 
-print client.get_children('/')
-print client.get_children('/zookeeper')
+zookeeper_client
+zookeeper_client.connect()
+
+print zookeeper_client.get_children('/')
+print zookeeper_client.get_children('/zookeeper')
+
+circus_client = CircusClient()
+
+def create_watcher(command):
+    message = {
+        'command': 'add',
+        'properties': {
+            'cmd': command,
+            'name': 'sleep',
+            'args': [],
+            'options': {},
+            'start': True
+        }
+    }
+    print circus_client.call(message)
 
 path = '/fairground/application/evo'
-while True:
-    print client.get(path)
-    time.sleep(1)
+try:
+    while True:
+        command, data = zookeeper_client.get(path)
+        print command, data
+        create_watcher(command)
+        time.sleep(30)
+finally:
+    zookeeper_client.stop()
